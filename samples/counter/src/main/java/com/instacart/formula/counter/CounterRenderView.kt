e53b45db2209b68f88e608f4d747c3a84c64c278
Laimonas Turauskas <laimiux@gmail.com>
2019-03-01 11:14:58 -0800

    Created simple counter example. (#6)
    * Created simple counter example.
    
    * Remove explicit invoke when calling lambdas.
    
    * Fix formatting + missing dependency.

---
+++ samples/counter/src/main/java/com/instacart/formula/counter/CounterRenderView.kt
@@ -0,0 +1,23 @@
+package com.instacart.formula.counter
+
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import com.instacart.formula.RenderView
+import com.instacart.formula.Renderer
+
+class CounterRenderView(private val root: ViewGroup): RenderView<CounterRenderModel> {
+    private val decrementButton: Button = root.findViewById(R.id.decrement_button)
+    private val incrementButton: Button = root.findViewById(R.id.increment_button)
+    private val countTextView: TextView = root.findViewById(R.id.count_text_view)
+
+    override val renderer: Renderer<CounterRenderModel> = Renderer.create { model ->
+        countTextView.setText(model.count)
+        decrementButton.setOnClickListener {
+            model.onDecrement()
+        }
+        incrementButton.setOnClickListener {
+            model.onIncrement()
+        }
+    }
+}
