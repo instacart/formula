7acf1765fa95c025e6ae0182fa6ff9724ffd287f
Laimonas Turauskas <laimiux@gmail.com>
2019-03-01 17:26:53 -0800

    Add more tests for FragmentFlowRenderView.

--- formula-integration-tests/src/main/java/com/instacart/formula/TestActivity.kt
+++ formula-integration-tests/src/main/java/com/instacart/formula/TestActivity.kt
@@ -3,20 +3,23 @@ package com.instacart.formula
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
 import com.instacart.formula.fragment.FormulaFragment
-import com.instacart.formula.fragment.FragmentFlowStore
 import com.instacart.formula.integration.FragmentFlowRenderView
+import io.reactivex.disposables.CompositeDisposable
 
 class TestActivity : FragmentActivity() {
+    private val disposables = CompositeDisposable()
 
+    // Exposed for testing purposes.
     lateinit var fragmentFlowRenderView: FragmentFlowRenderView
-
-    var store = FragmentFlowStore.init {  }
+    val component = TestComponent()
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        fragmentFlowRenderView = FragmentFlowRenderView(this, onLifecycleEvent = store::onLifecycleEffect)
+        fragmentFlowRenderView = FragmentFlowRenderView(this, onLifecycleEvent = component::onLifecycleEvent)
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.basic_integration_activity)
+
+        disposables.add(component.state.subscribe(fragmentFlowRenderView.renderer::render))
 
         if (savedInstanceState == null) {
             val contract = TaskListContract()
@@ -27,8 +30,9 @@ class TestActivity : FragmentActivity() {
     }
 
     override fun onDestroy() {
-        super.onDestroy()
+        disposables.clear()
         fragmentFlowRenderView.dispose()
+        super.onDestroy()
     }
 
     override fun onBackPressed() {
--- formula-integration-tests/src/test/java/com/instacart/formula/FragmentFlowRenderViewTest.kt
+++ formula-integration-tests/src/test/java/com/instacart/formula/FragmentFlowRenderViewTest.kt
@@ -1,10 +1,12 @@
 package com.instacart.formula
 
 import androidx.fragment.app.Fragment
-import androidx.test.rule.ActivityTestRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 import com.google.common.truth.Truth.assertThat
 import com.instacart.formula.fragment.FormulaFragment
+import com.instacart.formula.fragment.FragmentContract
+import com.instacart.formula.integration.BackCallback
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -17,7 +19,7 @@ class FragmentFlowRenderViewTest {
     @get:Rule val rule = ActivityTestRule(TestActivity::class.java)
 
     @Test fun `add fragment lifecycle event`() {
-        rule.activity.store.state().test().values().apply {
+        rule.activity.component.state.test().values().apply {
             assertThat(this.last().backStack.keys).containsExactly(
                 TaskListContract()
             )
@@ -25,45 +27,77 @@ class FragmentFlowRenderViewTest {
     }
 
     @Test fun `pop backstack lifecycle event`() {
-        rule.activity.store.state().test()
-            .apply {
-                navigateToTaskDetail()
-            }
-            .apply {
-                rule.activity.onBackPressed()
-            }
-            .values()
-            .apply {
-                assertThat(this.last().backStack.keys).containsExactly(TaskListContract())
-            }
+        navigateToTaskDetail()
+        rule.activity.onBackPressed()
+
+        assertThat(currentBackstack()).containsExactly(TaskListContract())
     }
 
     @Test fun `navigating forward should have both keys in backstack`() {
-        rule.activity.store.state().test()
-            .apply { navigateToTaskDetail() }
-            .values()
-            .apply {
-                assertThat(this.last().backStack.keys).containsExactly(
-                    TaskListContract(),
-                    TaskDetailContract(1)
-                )
-            }
+        navigateToTaskDetail()
+
+        assertThat(currentBackstack()).containsExactly(
+            TaskListContract(),
+            TaskDetailContract(1)
+        )
     }
 
     @Test fun `ignore headless fragments`() {
-        rule.activity.store.state().test()
-            .apply {
-                rule.activity.supportFragmentManager
-                    .beginTransaction()
-                    .add(HeadlessFragment(), "headless")
-                    .commitNow()
+        // add headless fragment
+        rule.activity.supportFragmentManager
+            .beginTransaction()
+            .add(HeadlessFragment(), "headless")
+            .commitNow()
+
+        assertThat(currentBackstack()).containsExactly(TaskListContract())
+    }
+
+    @Test fun `render model is passed to visible fragment`() {
+        val component = rule.activity.component
+        component.sendStateUpdate(TaskListContract(), "update")
+        assertThat(component.renderCalls).containsExactly(TaskListContract() to "update")
+    }
+
+    @Test fun `render model is not passed to not visible fragment`() {
+        navigateToTaskDetail()
+
+        val component = rule.activity.component
+        component.sendStateUpdate(TaskListContract(), "update")
+        assertThat(component.renderCalls).isEqualTo(emptyList<Any>())
+    }
+
+    @Test fun `visible fragments are updated when navigating`() {
+        navigateToTaskDetail()
+
+        val contract = TaskDetailContract(1)
+
+        val component = rule.activity.component
+        component.sendStateUpdate(contract, "update")
+        assertThat(component.renderCalls).containsExactly(contract to "update")
+
+        rule.activity.onBackPressed()
+
+        component.sendStateUpdate(contract, "update-two")
+        assertThat(component.renderCalls).containsExactly(contract to "update")
+    }
+
+    @Test fun `delegates back press to current render model`() {
+        navigateToTaskDetail()
+
+        var backPressed = 0
+
+        val contract = TaskDetailContract(1)
+        val component = rule.activity.component
+        component.sendStateUpdate(contract, object : BackCallback {
+            override fun onBackPressed() {
+                backPressed += 1
             }
-            .values()
-            .apply {
-                assertThat(this.last().backStack.keys).containsExactly(
-                    TaskListContract()
-                )
-            }
+        })
+
+        rule.activity.onBackPressed()
+        rule.activity.onBackPressed()
+
+        assertThat(backPressed).isEqualTo(2)
     }
 
     private fun navigateToTaskDetail() {
@@ -73,5 +107,9 @@ class FragmentFlowRenderViewTest {
             .add(R.id.activity_content, FormulaFragment.newInstance(detail), detail.tag)
             .addToBackStack(null)
             .commit()
+    }
+
+    private fun currentBackstack(): List<FragmentContract<*>> {
+        return rule.activity.component.currentFragmentState().backStack.keys
     }
 }
