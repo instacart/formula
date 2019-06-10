1548837e19c63fe6a00779b2f4cbe9f95dfa0190
Laimonas Turauskas <laimiux@gmail.com>
2019-06-10 13:34:08 -0700

    Use onInitActivity to do initial contract injection.

--- formula-integration-tests/src/main/java/com/instacart/formula/TestFragmentActivity.kt
+++ formula-integration-tests/src/main/java/com/instacart/formula/TestFragmentActivity.kt
@@ -2,22 +2,23 @@ package com.instacart.formula
 
 import android.os.Bundle
 import com.instacart.formula.fragment.FormulaFragment
+import com.instacart.formula.fragment.FragmentContract
 import com.instacart.formula.integration.FormulaAppCompatActivity
 
 class TestFragmentActivity : FormulaAppCompatActivity() {
-
-    lateinit var contract: TestLifecycleContract
+    // Exposed for testing purposes.
+    lateinit var initialContract: FragmentContract<*>
+    val renderCalls = mutableListOf<Pair<FragmentContract<*>, *>>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.test_activity)
 
         if (savedInstanceState == null) {
-            val contract = TestLifecycleContract()
+            val fragment = FormulaFragment.newInstance(initialContract)
             supportFragmentManager.beginTransaction()
-                .add(R.id.activity_content, FormulaFragment.newInstance(contract), contract.tag)
+                .add(R.id.activity_content, fragment, initialContract.tag)
                 .commit()
-            this.contract = contract
         }
     }
 }
