5f42a1291e5dad98fd0fa0f58751672b0191c593
Laimonas Turauskas <laimiux@gmail.com>
2019-07-12 18:21:10 -0700

    Fix todoapp compilation issue.

--- samples/todoapp/src/main/java/com/examples/todoapp/TodoApp.kt
+++ samples/todoapp/src/main/java/com/examples/todoapp/TodoApp.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.examples.todoapp.tasks.TaskListContract
 import com.examples.todoapp.tasks.TaskListFormula
 import com.instacart.formula.FormulaAndroid
+import com.instacart.formula.state
 
 class TodoApp : Application() {
 
