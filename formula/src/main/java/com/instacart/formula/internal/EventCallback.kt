d85b7abf37186d0193c12477705b0dccd8a31d65
Laimonas Turauskas <laimiux@gmail.com>
2019-08-05 11:38:03 -0700

    Inlined callback lamba construction.

--- formula/src/main/java/com/instacart/formula/internal/EventCallback.kt
+++ formula/src/main/java/com/instacart/formula/internal/EventCallback.kt
@@ -4,7 +4,7 @@ package com.instacart.formula.internal
  * Note: this class is not a data class because equality is based on instance and not [key].
  */
 class EventCallback<T>(val key: Any): (T) -> Unit {
-    internal lateinit var callback: (T) -> Unit
+    @PublishedApi internal lateinit var callback: (T) -> Unit
 
     override fun invoke(p1: T) {
         callback(p1)
