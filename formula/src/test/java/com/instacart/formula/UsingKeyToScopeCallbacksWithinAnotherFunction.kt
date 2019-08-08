a23d643099cc983a7e03c8a479125b2c5d4eb4fb
Laimonas Turauskas <laimiux@gmail.com>
2019-08-08 10:04:35 -0700

    Adding tests for a crashing scenario and how to work around it.

---
+++ formula/src/test/java/com/instacart/formula/UsingKeyToScopeCallbacksWithinAnotherFunction.kt
@@ -0,0 +1,36 @@
+package com.instacart.formula
+
+object UsingKeyToScopeCallbacksWithinAnotherFunction {
+
+    class ChildRenderModel(
+        val callback: () -> Unit
+    )
+
+    class TestRenderModel(
+        val first: ChildRenderModel,
+        val second: ChildRenderModel
+    )
+
+    class TestFormula : StatelessFormula<Unit, Unit, TestRenderModel>() {
+        override fun evaluate(input: Unit, context: FormulaContext<Unit, Unit>): Evaluation<TestRenderModel> {
+            return Evaluation(
+                renderModel = TestRenderModel(
+                    first = context.key("first") {
+                        createChild(context)
+                    },
+                    second = context.key("second") {
+                        createChild(context)
+                    }
+                )
+            )
+        }
+
+        private fun createChild(context: FormulaContext<Unit, Unit>): ChildRenderModel {
+            return ChildRenderModel(
+                callback = context.callback {
+                    none()
+                }
+            )
+        }
+    }
+}
