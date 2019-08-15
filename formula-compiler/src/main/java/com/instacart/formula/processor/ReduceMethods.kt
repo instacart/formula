0750cc38091834879f6b0405c734f2857978c2c4
Laimonas Turauskas <laimiux@gmail.com>
2019-08-15 08:17:03 -0700

    Removing legacy stuff. (#100)
    * Removing legacy code.
    
    * Update CI script.
    
    * Use assembleRelease for sample apps.

--- formula-compiler/src/main/java/com/instacart/formula/processor/ReduceMethods.kt
+++
@@ -1,8 +0,0 @@
-package com.instacart.formula.processor
-
-import com.squareup.kotlinpoet.TypeName
-
-/**
- * All reduce methods should have the same return type
- */
-class ReduceMethods(val returnType: TypeName, val methods: List<ReduceMethod>)
--- formula-legacy/src/main/java/com/instacart/formula/NextReducer.kt
+++
@@ -1,6 +0,0 @@
-package com.instacart.formula
-
-/**
- * Simple typealias for [Next], denoted as a reducer
- */
-typealias NextReducer<State, Effect> = (State) -> Next<State, Effect>
