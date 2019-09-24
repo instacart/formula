b85ceb9f4957c9605dd951272b8613c5cfa4e1cf
Laimonas Turauskas <laimiux@gmail.com>
2019-09-24 16:18:15 -0700

    [samples] add a stopwatch sample. (#108)
    * [samples] add a stopwatch sample.
    
    * Don't show ms.

---
+++ samples/stopwatch/src/main/java/com/instacart/formula/stopwatch/StopwatchFormula.kt
@@ -0,0 +1,88 @@
+package com.instacart.formula.stopwatch
+
+import com.instacart.formula.Evaluation
+import com.instacart.formula.Formula
+import com.instacart.formula.FormulaContext
+import com.instacart.formula.RxStream
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import java.util.concurrent.TimeUnit
+
+class StopwatchFormula : Formula<Unit, StopwatchFormula.State, StopwatchRenderModel> {
+
+    data class State(
+        val timePassedInMillis: Long,
+        val isRunning: Boolean
+    )
+
+    override fun initialState(input: Unit): State = State(
+        timePassedInMillis = 0,
+        isRunning = false
+    )
+
+    override fun evaluate(
+        input: Unit,
+        state: State,
+        context: FormulaContext<State>
+    ): Evaluation<StopwatchRenderModel> {
+        return Evaluation(
+            renderModel = StopwatchRenderModel(
+                timePassed = formatTimePassed(state.timePassedInMillis),
+                startStopButton = startStopButton(state, context),
+                resetButton = resetButton(state, context)
+            ),
+            updates = context.updates {
+                if (state.isRunning) {
+                    val incrementTimePassed = RxStream.fromObservable {
+                        Observable.interval(1, TimeUnit.MILLISECONDS).observeOn(AndroidSchedulers.mainThread())
+                    }
+
+                    events(incrementTimePassed) {
+                        transition(state.copy(timePassedInMillis = state.timePassedInMillis + 1))
+                    }
+                }
+            }
+        )
+    }
+
+    private fun formatTimePassed(timePassedInMillis: Long): String {
+        return buildString {
+            val minutesPassed = TimeUnit.MILLISECONDS.toMinutes(timePassedInMillis)
+            if (minutesPassed > 0) {
+                append(minutesPassed)
+                append('m')
+                append(' ')
+            }
+
+            val secondsPassed = TimeUnit.MILLISECONDS.toSeconds(timePassedInMillis) % 60
+            append(secondsPassed)
+            append('s')
+            append(' ')
+
+            // Always show millis as two digits
+            val millisPassed = (timePassedInMillis % 1000) / 10
+            if (millisPassed < 10) {
+                append('0')
+            }
+            append(millisPassed)
+        }
+    }
+
+    private fun startStopButton(state: State, context: FormulaContext<State>): ButtonRenderModel {
+        return ButtonRenderModel(
+            text = if (state.isRunning) "Stop" else "Start",
+            onSelected = context.callback {
+                transition(state.copy(isRunning = !state.isRunning))
+            }
+        )
+    }
+
+    private fun resetButton(state: State, context: FormulaContext<State>): ButtonRenderModel {
+        return ButtonRenderModel(
+            text = "Reset",
+            onSelected = context.callback {
+                transition(state.copy(timePassedInMillis = 0, isRunning = false))
+            }
+        )
+    }
+}
---
+++ samples/stopwatch/src/main/java/com/instacart/formula/stopwatch/StopwatchRenderView.kt
@@ -0,0 +1,27 @@
+package com.instacart.formula.stopwatch
+
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import com.instacart.formula.RenderView
+import com.instacart.formula.Renderer
+
+class StopwatchRenderView(root: ViewGroup): RenderView<StopwatchRenderModel> {
+    private val timePassed: TextView = root.findViewById(R.id.time_passed_text_view)
+    private val startStopButton: Button = root.findViewById(R.id.start_stop_button)
+    private val resetButton: Button = root.findViewById(R.id.reset_button)
+
+    override val renderer: Renderer<StopwatchRenderModel> = Renderer.create { model ->
+        timePassed.text = model.timePassed
+
+        startStopButton.text = model.startStopButton.text
+        startStopButton.setOnClickListener {
+            model.startStopButton.onSelected()
+        }
+
+        resetButton.text = model.resetButton.text
+        resetButton.setOnClickListener {
+            model.resetButton.onSelected()
+        }
+    }
+}
