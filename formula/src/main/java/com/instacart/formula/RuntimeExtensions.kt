645eef5939e21630df9f7030c273b1b9fbb72c18
John Carlson <jawnnypoo@gmail.com>
2020-01-28 11:53:58 -0600

    Remove deprecated state extension (#122)

--- formula/src/main/java/com/instacart/formula/RuntimeExtensions.kt
+++ formula/src/main/java/com/instacart/formula/RuntimeExtensions.kt
@@ -17,17 +17,3 @@ fun <Input : Any, State, RenderModel : Any> Formula<Input, State, RenderModel>.s
 ): Observable<RenderModel> {
     return FormulaRuntime.start(input = input, formula = this)
 }
-
-@Deprecated(message = "Use start instead of state", replaceWith = ReplaceWith("start(input)", "com.instacart.formula.start"))
-fun <Input : Any, State, RenderModel : Any> Formula<Input, State, RenderModel>.state(
-    input: Input
-): Observable<RenderModel> {
-    return start(input = Observable.just(input))
-}
-
-@Deprecated(message = "Use start instead of state", replaceWith = ReplaceWith("start(input)", "com.instacart.formula.start"))
-fun <Input : Any, State, RenderModel : Any> Formula<Input, State, RenderModel>.state(
-    input: Observable<Input>
-): Observable<RenderModel> {
-    return FormulaRuntime.start(input = input, formula = this)
-}
