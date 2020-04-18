244de97e5bf7a02da1933df26a50d0a5b5ec4128
Laimonas Turauskas <laimiux@gmail.com>
2020-04-17 17:06:22 -0700

    [sample] creating a custom stream implementation.

---
+++ samples/custom-network-state-stream/src/main/java/com/instacart/formula/samples/networkstate/NetworkStateActivity.kt
@@ -0,0 +1,28 @@
+package com.instacart.formula.samples.networkstate
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.fragment.app.FragmentActivity
+import com.instacart.formula.start
+import io.reactivex.disposables.CompositeDisposable
+
+class NetworkStateActivity : FragmentActivity() {
+
+    private val disposables = CompositeDisposable()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+        val networkStatusTextView = findViewById<TextView>(R.id.network_status_text)
+        val renderModels = NetworkStateFormula(NetworkStateStreamImpl(application)).start()
+        disposables.add(renderModels.subscribe {
+            networkStatusTextView.text = it.status
+        })
+    }
+
+    override fun onDestroy() {
+        disposables.clear()
+        super.onDestroy()
+    }
+}
---
+++ samples/custom-network-state-stream/src/main/java/com/instacart/formula/samples/networkstate/NetworkStateStreamImpl.kt
@@ -0,0 +1,40 @@
+package com.instacart.formula.samples.networkstate
+
+import android.app.Application
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import com.instacart.formula.Cancelable
+
+class NetworkStateStreamImpl(private val application: Application) : NetworkStateStream {
+
+    override fun start(send: (NetworkState) -> Unit): Cancelable? {
+        // Broadcast receiver setup
+        val action = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        val receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                send(getNetworkState(application))
+            }
+        }
+
+        // Register receiver
+        application.registerReceiver(receiver, action, null, null);
+
+        // Emit initial state
+        send(getNetworkState(application))
+
+        return Cancelable {
+            // Unregister
+            application.unregisterReceiver(receiver)
+        }
+    }
+
+    private fun getNetworkState(context: Context): NetworkState {
+        val cm: ConnectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = cm.activeNetworkInfo
+        val isOnline = null != activeNetwork && activeNetwork.isConnected
+        return NetworkState(isOnline)
+    }
+}
