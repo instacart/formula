487322e24e2b52e2b0e60e3230f29e475b05a01b
John Carlson <jawnnypoo@gmail.com>
2020-06-30 16:59:03 -0500

    Mockk extension example

--- samples/todoapp/src/test/java/com/examples/todoapp/tasks/TaskListFormulaTest.kt
+++ samples/todoapp/src/test/java/com/examples/todoapp/tasks/TaskListFormulaTest.kt
@@ -3,21 +3,21 @@ package com.examples.todoapp.tasks
 import com.examples.todoapp.data.Task
 import com.examples.todoapp.data.TaskRepo
 import com.google.common.truth.Truth.assertThat
+import com.instacart.formula.start
 import com.instacart.formula.test.test
-import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.whenever
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
 import io.reactivex.rxjava3.core.Observable
 import org.junit.Test
 
 class TaskListFormulaTest {
 
     @Test fun `change filter type`() {
-        val repo = mock<TaskRepo>()
-        whenever(repo.tasks()).thenReturn(Observable.just(
-            listOf(
-                Task("Mow the lawn."),
-                Task("Go get a haircut.")
-            )
+        val repo = mockk<TaskRepo>()
+        every { repo.tasks() } returns Observable.just(listOf(
+            Task("Mow the lawn."),
+            Task("Go get a haircut.")
         ))
 
         TaskListFormula(repo)
@@ -32,4 +32,21 @@ class TaskListFormulaTest {
                 assertThat(items).isEmpty()
             }
     }
+
+    @Test fun `change formula output`() {
+        val formula = mockk<TaskListFormula>()
+        val filterOption = TaskFilterRenderModel(title = "test", onSelected = {})
+        mockkStatic("com.instacart.formula.RuntimeExtensionsKt")
+        every { formula.start(any<TaskListFormula.Input>()) } returns Observable.just(
+            TaskListRenderModel(items = emptyList(), filterOptions = listOf(filterOption))
+        )
+        formula.start(TaskListFormula.Input { })
+            .test()
+            .assertValueAt(0) {
+                it.items.isEmpty() &&
+                    it.filterOptions.size == 1 &&
+                    it.filterOptions.first().title == "test"
+            }
+    }
+
 }
