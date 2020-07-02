29d69e530cef0ca79c9e4563de8d91a39c2a5542
John Carlson <jawnnypoo@gmail.com>
2020-07-02 15:21:41 -0500

    Try the percentage again

--- jacoco.sh
+++ jacoco.sh
@@ -1,2 +1,8 @@
-./test.sh
-./gradlew jacocoTestReportMerged --stacktrace
+#!/bin/bash
+# Should match with what we run in .circleci/config.yml
+./gradlew clean
+./gradlew :formula:test
+./gradlew :formula-android:testRelease
+./gradlew :formula-android-tests:testRelease
+./gradlew jacocoTestReportMerged
+open build/reports/jacoco/index.html
