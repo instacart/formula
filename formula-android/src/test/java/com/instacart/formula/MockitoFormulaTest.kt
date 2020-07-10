3c537de5a1ff8bfdb30e6253846da153bf581e56
Laimonas Turauskas <laimiux@gmail.com>
2020-07-09 19:30:37 -0700

    Fixing mockito example. (#156)

--- formula-android/src/test/java/com/instacart/formula/MockitoFormulaTest.kt
+++ formula-android/src/test/java/com/instacart/formula/MockitoFormulaTest.kt
@@ -1,15 +1,17 @@
 package com.instacart.formula
 
 import com.google.common.truth.Truth.assertThat
+import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.spy
 import com.nhaarman.mockito_kotlin.whenever
 import org.junit.Test
+import org.mockito.internal.stubbing.answers.CallsRealMethods
 
 class MockitoFormulaTest {
 
     @Test fun mockMaintainsType() {
         val testFormula = ReplacementFormula().implementation()
-        val formula = spy<MyFormula>()
+        val formula = mock<MyFormula>(defaultAnswer = CallsRealMethods())
         whenever(formula.implementation()).thenReturn(testFormula)
         assertThat(formula.implementation()).isEqualTo(testFormula)
         assertThat(formula.type()).isEqualTo(MyFormula::class)
