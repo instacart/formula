3eaecfc69368e962a6a633f71691ff94180c882f
Laimonas Turauskas <laimiux@gmail.com>
2020-11-17 13:07:34 -0800

    Bugfix: duplicate key is added on activity rotation. (#167)
    * Bugfix: duplicate key is added on activity rotation.
    
    * Add test to capture issue.

--- formula-android-tests/src/test/java/com/instacart/formula/TestFormulaRule.kt
+++ formula-android-tests/src/test/java/com/instacart/formula/TestFormulaRule.kt
@@ -2,8 +2,11 @@ package com.instacart.formula
 
 import android.app.Application
 import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth
+import io.reactivex.rxjava3.plugins.RxJavaPlugins
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
+import org.junit.runners.model.Statement
 
 /**
  * A rule to initialize FormulaAndroid.
@@ -12,6 +15,21 @@ class TestFormulaRule(
     private val initFormula: (Application) -> Unit,
     private val cleanUp: () -> Unit = {}
 ) : TestWatcher() {
+    private val errors = mutableListOf<Throwable>()
+
+    override fun apply(base: Statement, description: Description): Statement {
+        RxJavaPlugins.reset()
+        RxJavaPlugins.setErrorHandler { errors.add(it) }
+
+        try {
+            val result = super.apply(base, description)
+            assertNoErrors()
+            return result
+        } finally {
+            errors.clear()
+            RxJavaPlugins.reset()
+        }
+    }
 
     override fun starting(description: Description?) {
         super.starting(description)
@@ -32,5 +50,9 @@ class TestFormulaRule(
     private fun initializeFormula() {
         val context = ApplicationProvider.getApplicationContext<Application>()
         initFormula(context)
+    }
+
+    private fun assertNoErrors() {
+        Truth.assertThat(errors).isEmpty()
     }
 }
