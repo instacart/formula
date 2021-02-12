3b19514eaddedda3019b68b25b14bb3d7b9cd434
Mathieu Hartvick <mathieu.hartvick@instacart.com>
2021-02-11 20:52:44 -0500

    remove log import

--- formula-android/src/main/java/com/instacart/formula/integration/FragmentFlowRenderView.kt
+++ formula-android/src/main/java/com/instacart/formula/integration/FragmentFlowRenderView.kt
@@ -2,7 +2,6 @@ package com.instacart.formula.integration
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
