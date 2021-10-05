59cb7084f06ac8bdfcd84da0101a46b960631511
Laimonas Turauskas <laimiux@gmail.com>
2021-10-05 15:37:04 -0700

    Clean up TransitionListener.

--- formula/src/main/java/com/instacart/formula/internal/TransitionListener.kt
+++ formula/src/main/java/com/instacart/formula/internal/TransitionListener.kt
@@ -2,16 +2,6 @@ package com.instacart.formula.internal
 
 import com.instacart.formula.Transition
 
-interface TransitionListener {
-    companion object {
-        inline operator fun invoke(crossinline listener: (Transition<*>, isValid: Boolean) -> Unit): TransitionListener {
-            return object : TransitionListener {
-                override fun onTransition(transition: Transition<*>, isValid: Boolean) {
-                    listener(transition, isValid)
-                }
-            }
-        }
-    }
-
+internal fun interface TransitionListener {
     fun onTransition(transition: Transition<*>, isValid: Boolean)
 }
