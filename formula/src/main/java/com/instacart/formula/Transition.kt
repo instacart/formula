e194d14d3a1c953bb17cd0d921f484c9e2d7a011
Laimonas Turauskas <laimiux@gmail.com>
2021-10-06 13:49:05 -0700

    Documentation.

--- formula/src/main/java/com/instacart/formula/Transition.kt
+++ formula/src/main/java/com/instacart/formula/Transition.kt
@@ -2,47 +2,63 @@ package com.instacart.formula
 
 /**
  * Transition is a function that is called when an [Event] happens and produces a [Result] which
- * can indicate a [state][State] change, [side-effects][Result.effects] or nothing. This result
- * will be passed and applied by the [Formula Runtime][FormulaRuntime].
+ * indicates what [Formula] should do in response to this event. It can contain a new [state][State]
+ * object which will trigger [Formula.evaluate] to be called and/or an [effects][Effects] function
+ * which will be executed by the [FormulaRuntime]. If there was a state change, effects function
+ * will be executed after [Formula.evaluate] is called.
  */
 fun interface Transition<State, in Event> {
 
     /**
-     * Defines a transition result which can request a state change and/or some action
-     * to be performed.
+     * Result is an object returned by [Transition.toResult] which indicates what
+     * [Formula] should do in response to an event. It can contain a new [state][Stateful]
+     * and/or an [effects][Effects] function which will be executed by the [FormulaRuntime].
+     * Usually, you should use [TransitionContext] to construct the [Result] object.
      */
     sealed class Result<out State> {
+
         /**
-         * Transition result requesting a state change and option [effects] to be executed.
+         * Stateful result returned by the transition indicates that we want to apply a
+         * new state to our [Formula]. It can also return optional [effects] function
+         * which will be executed by [FormulaRuntime] after the state change.
          *
-         * @param state New state
-         * @param effects Optional effects such as calling listeners, logging, db writes,
-         * network requests, etc.
+         * @param state New state which will be applied to our [Formula].
+         * @param effects Optional function which will be executed after the state change. This
+         * function is the place to call listeners, log events, trigger network requests or
+         * database writes, and etc.
          */
         data class Stateful<State>(val state: State, override val effects: Effects? = null) : Result<State>()
 
         /**
-         * Transition result requesting [effects] to be executed.
+         * Only effects result returned by the transition indicates that we don't need to update
+         * [Formula] state and only want [effects] function to be executed by [FormulaRuntime].
          *
-         * @param effects Effects such as calling listeners, logging, db writes, network requests, etc.
+         * @param effects A function which will be executed by [FormulaRuntime]. This function
+         * is the place to call listeners, log events, trigger network requests or database
+         * writes, and etc.
          */
         data class OnlyEffects(override val effects: Effects) : Result<Nothing>()
 
         /**
-         * Nothing happens in this transition.
+         * None result returned by the transition indicates that [Formula] doesn't need to
+         * do anything in response to an event.
          */
         object None : Result<Nothing>() {
             override val effects: Effects? = null
         }
 
         /**
-         * Optional side-effects function that will be executed by [Formula Runtime][FormulaRuntime].
+         * Effects is an optional function returned by [toResult] which will be executed by
+         * [FormulaRuntime]. This function is the place to call listeners, log events, trigger
+         * network requests or database writes, and etc.
+         *
+         * @see Effects
          */
         abstract val effects: Effects?
     }
 
     /**
-     * Called when an [Event] happens and creates a [Result] type which can indicate a state
+     * Called when an [Event] happens and returns a [Result] object which can indicate a state
      * change and/or some executable effects. Use [TransitionContext.none] if nothing should happen
      * as part of this event.
      */
