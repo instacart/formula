eaa61a2c147919ac3dd4646ae7112e589501d264
Laimonas Turauskas <laimiux@gmail.com>
2022-06-22 09:42:02 +0300

    [Core] Add auto indexing of keys.

--- formula/src/test/java/com/instacart/formula/internal/Try.kt
+++ formula/src/test/java/com/instacart/formula/internal/Try.kt
@@ -3,7 +3,7 @@ package com.instacart.formula.internal
 sealed class Try<out T> {
 
     data class Error(val throwable: Throwable): Try<Nothing>() {
-        override fun errorOrNull(): Throwable? = throwable
+        override fun errorOrNull(): Throwable = throwable
     }
 
     data class Result<out T>(val data: T): Try<T>() {
---
+++ formula/src/test/java/com/instacart/formula/subjects/DuplicateListenerKeysHandledByIndexing.kt
@@ -0,0 +1,29 @@
+package com.instacart.formula.subjects
+
+import com.instacart.formula.Evaluation
+import com.instacart.formula.Listener
+import com.instacart.formula.Snapshot
+import com.instacart.formula.StatelessFormula
+import com.instacart.formula.test.TestableRuntime
+
+object DuplicateListenerKeysHandledByIndexing {
+
+    fun test(runtime: TestableRuntime) = runtime.test(TestFormula(), Unit)
+
+    class TestOutput(
+        val listeners: List<Listener<Unit>>,
+    )
+
+    class TestFormula : StatelessFormula<Unit, TestOutput>() {
+        override fun Snapshot<Unit, Unit>.evaluate(): Evaluation<TestOutput> {
+            val keys = listOf(1, 2, 1)
+            return Evaluation(
+                output = TestOutput(
+                    listeners = keys.map { key ->
+                        context.onEvent(key) { none() }
+                    }
+                )
+            )
+        }
+    }
+}
