8dd7788806608bb5cc40bfbe2fd447a17b104e5b
John Carlson <jawnnypoo@gmail.com>
2023-06-27 16:51:29 -0500

    A few more fixes

--- formula-android/src/test/java/com/instacart/formula/android/fakes/NoOpViewFactory.kt
+++ formula-android/src/test/java/com/instacart/formula/android/fakes/NoOpViewFactory.kt
@@ -4,7 +4,7 @@ import com.instacart.formula.android.FeatureView
 import com.instacart.formula.android.LayoutViewFactory
 import com.instacart.formula.android.ViewInstance
 
-class NoOpViewFactory<RenderModel> : LayoutViewFactory<RenderModel>(-1) {
+class NoOpViewFactory<RenderModel : Any> : LayoutViewFactory<RenderModel>(-1) {
     override fun ViewInstance.create(): FeatureView<RenderModel> {
         return featureView {  }
     }
