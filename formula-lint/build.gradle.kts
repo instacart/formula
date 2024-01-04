7437149d21ef753be46dfc9192dcca8991b59da3
Laimonas Turauskas <laimiux@gmail.com>
2024-01-04 10:05:50 -0500

    Fix lint build.

--- formula-lint/build.gradle.kts
+++ formula-lint/build.gradle.kts
@@ -1,3 +1,6 @@
+import com.android.build.gradle.internal.lint.AndroidLintAnalysisTask
+import com.android.build.gradle.internal.lint.LintModelWriterTask
+
 plugins {
   id("java-library")
   id("kotlin")
@@ -18,4 +21,13 @@ dependencies {
   testImplementation(libs.lint.core)
   testImplementation(libs.lint.tests)
   testImplementation(libs.junit)
+}
+
+// Need to register direct task dependencies since jacocoTestReport is
+// accessing the files produced by those lint tasks
+plugins.withId("jacoco") {
+  tasks.named("jacocoTestReport") {
+    dependsOn(tasks.withType(AndroidLintAnalysisTask::class.java))
+    dependsOn(tasks.withType(LintModelWriterTask::class.java))
+  }
 }
