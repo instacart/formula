2006bed0c364cf956aabf5649e4d9eb4b6776e84
John Carlson <jawnnypoo@gmail.com>
2024-01-26 14:38:04 -0600

    Fix lint test

--- formula-lint/src/test/java/com/instacart/formula/lint/WrongFormulaUsageDetectorTest.kt
+++ formula-lint/src/test/java/com/instacart/formula/lint/WrongFormulaUsageDetectorTest.kt
@@ -4,7 +4,9 @@ import com.android.tools.lint.checks.infrastructure.TestFile
 import com.android.tools.lint.checks.infrastructure.TestFiles.kotlin
 import com.android.tools.lint.checks.infrastructure.TestLintResult
 import com.android.tools.lint.checks.infrastructure.TestLintTask.lint
+import com.android.tools.lint.checks.infrastructure.TestMode
 import com.instacart.formula.lint.WrongFormulaUsageDetector.Companion.issues
+import com.intellij.lang.PsiBuilderUtil.expect
 import org.junit.Test
 
 class WrongFormulaUsageDetectorTest {
@@ -95,6 +97,7 @@ class WrongFormulaUsageDetectorTest {
         return lint()
             .files(*array)
             .issues(*issues)
+            .skipTestModes(TestMode.SUPPRESSIBLE)
             .run()
     }
 
