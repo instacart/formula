b2db74fe9025f8faaf503d63429e295352b57902
Laimonas Turauskas <laimiux@gmail.com>
2024-03-05 17:09:16 -0500

    Add tests for various execution types.

--- formula/src/test/java/com/instacart/formula/subjects/EventCallbackFormula.kt
+++ formula/src/test/java/com/instacart/formula/subjects/EventCallbackFormula.kt
@@ -3,8 +3,11 @@ package com.instacart.formula.subjects
 import com.instacart.formula.Evaluation
 import com.instacart.formula.Formula
 import com.instacart.formula.Snapshot
+import com.instacart.formula.Transition
 
-class EventCallbackFormula : Formula<Unit, String, EventCallbackFormula.Output>() {
+class EventCallbackFormula(
+    private val executionType: Transition.ExecutionType? = null
+) : Formula<Unit, String, EventCallbackFormula.Output>() {
 
     data class Output(
         val state: String,
@@ -14,12 +17,19 @@ class EventCallbackFormula : Formula<Unit, String, EventCallbackFormula.Output>(
     override fun initialState(input: Unit): String = ""
 
     override fun Snapshot<Unit, String>.evaluate(): Evaluation<Output> {
+        val changeState: (String) -> Unit = if (executionType == null) {
+            context.onEvent { newState ->
+                transition(newState)
+            }
+        } else {
+            context.onEventWithExecutionType(executionType) { newState ->
+                transition(newState)
+            }
+        }
         return Evaluation(
             output = Output(
                 state = state,
-                changeState = context.onEvent { newState ->
-                    transition(newState)
-                }
+                changeState = changeState,
             )
         )
     }
