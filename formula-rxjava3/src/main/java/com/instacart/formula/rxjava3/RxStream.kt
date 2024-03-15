a83c927ed8a0eb6aecb259f91d7f1f5c109e468d
Laimonas Turauskas <laimiux@gmail.com>
2024-03-15 17:03:19 -0400

    Removing RxStream typealias.

--- formula-rxjava3/src/main/java/com/instacart/formula/rxjava3/RxStream.kt
+++ formula-rxjava3/src/main/java/com/instacart/formula/rxjava3/RxStream.kt
@@ -2,8 +2,3 @@
 
 package com.instacart.formula.rxjava3
 
-@Deprecated(
-    "use RxAction",
-    replaceWith = ReplaceWith("RxAction", "com.instacart.formula.rxjava3.RxAction")
-)
-typealias RxStream<Event> = RxAction<Event>
