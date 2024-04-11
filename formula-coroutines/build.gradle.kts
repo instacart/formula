912fcac3141e50e5380d3a8c2d3cdaac259f6c11
John Carlson <jawnnypoo@gmail.com>
2024-04-11 15:19:19 -0500

    Publish coroutine support

--- formula-coroutines/build.gradle.kts
+++ formula-coroutines/build.gradle.kts
@@ -3,6 +3,10 @@ plugins {
     id("kotlin")
 }
 
+apply {
+    from("$rootDir/.buildscript/configure-signing.gradle")
+}
+
 dependencies {
     implementation(libs.kotlin)
     implementation(libs.coroutines)
