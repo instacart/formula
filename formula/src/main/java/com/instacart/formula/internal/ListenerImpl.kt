6491f0a808213886ffa5d8e4c5e43beedf67ae87
Laimonas Turauskas <laimiux@gmail.com>
2024-09-12 16:46:32 -0400

    Increase code coverage (pt3).

--- formula/src/main/java/com/instacart/formula/internal/ListenerImpl.kt
+++ formula/src/main/java/com/instacart/formula/internal/ListenerImpl.kt
@@ -8,13 +8,14 @@ import com.instacart.formula.plugin.Dispatcher
  * Note: this class is not a data class because equality is based on instance and not [key].
  */
 @PublishedApi
-internal class ListenerImpl<Input, State, EventT>(private val key: Any) : Listener<EventT> {
+internal class ListenerImpl<Input, State, EventT>(
+    private val key: Any,
+    private var transition: Transition<Input, State, EventT>
+) : Listener<EventT> {
 
     @Volatile private var manager: FormulaManagerImpl<Input, State, *>? = null
     @Volatile private var snapshotImpl: SnapshotImpl<Input, State>? = null
     @Volatile private var executionType: Transition.ExecutionType? = null
-
-    private lateinit var transition: Transition<Input, State, EventT>
 
     override fun invoke(event: EventT) {
         // TODO: log if null listener (it might be due to formula removal or due to callback removal)
