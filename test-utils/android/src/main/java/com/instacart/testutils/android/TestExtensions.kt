94b3ceab79207c085089fe1144b22ecd52b67a7e
Laimonas Turauskas <laimiux@gmail.com>
2024-09-19 13:47:47 -0400

    Refactoring android tests to use utils/android (pt2). (#394)

--- test-utils/android/src/main/java/com/instacart/testutils/android/TestExtensions.kt
+++ test-utils/android/src/main/java/com/instacart/testutils/android/TestExtensions.kt
@@ -1,8 +1,15 @@
 package com.instacart.testutils.android
 
 import android.app.Activity
+import android.app.Application
 import android.os.Looper
 import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
+import com.instacart.formula.FormulaAndroid
+import com.instacart.formula.android.ActivityConfigurator
+import com.instacart.formula.android.FragmentEnvironment
+import io.reactivex.rxjava3.plugins.RxJavaPlugins
 import org.robolectric.Shadows
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.Executors
@@ -34,4 +41,24 @@ fun executeOnBackgroundThread(action: () -> Unit) {
     }
     initLatch.throwOnTimeout()
     Shadows.shadowOf(Looper.getMainLooper()).idle()
+}
+
+fun withFormulaAndroid(
+    environment: FragmentEnvironment = FragmentEnvironment(),
+    configure: ActivityConfigurator.() -> Unit = {},
+    continuation: () -> Unit,
+) {
+    val errors = mutableListOf<Throwable>()
+    RxJavaPlugins.reset()
+    RxJavaPlugins.setErrorHandler { errors.add(it) }
+
+    try {
+        val context = ApplicationProvider.getApplicationContext<Application>()
+        FormulaAndroid.init(context, environment, configure)
+        continuation()
+    } finally {
+        RxJavaPlugins.reset()
+        FormulaAndroid.reset()
+        assertThat(errors).isEmpty()
+    }
 }
