1c901ef7dfb10989a0071f3256c1909fe72e7182
Laimonas Turauskas <laimiux@gmail.com>
2025-05-06 12:15:41 -0400

    Add CoroutineAction to support suspend functions.

---
+++ formula-coroutines/src/main/java/com/instacart/formula/coroutines/CoroutineExtensions.kt
@@ -0,0 +1,18 @@
+package com.instacart.formula.coroutines
+
+import kotlinx.coroutines.CancellationException
+
+@PublishedApi
+internal suspend inline fun <Result> runCatchingCoroutines(
+    crossinline action: suspend () -> Result
+): kotlin.Result<Result> {
+    return try {
+        val result = action()
+        kotlin.Result.success(result)
+    } catch (e: CancellationException) {
+        // We need to emit cancellation errors
+        throw e
+    } catch (e: Throwable) {
+        kotlin.Result.failure(e)
+    }
+}
