62bd47f6f39913a377266928b9adecfd63abfcef
Laimonas Turauskas <laimiux@gmail.com>
2025-05-30 13:27:58 -0400

    Revert toObservable migration.

---
+++ formula-rxjava3/src/main/java/com/instacart/formula/rxjava3/RxJavaRuntime.kt
@@ -0,0 +1,31 @@
+package com.instacart.formula.rxjava3
+
+import com.instacart.formula.FormulaRuntime
+import com.instacart.formula.IFormula
+import com.instacart.formula.RuntimeConfig
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
+
+object RxJavaRuntime {
+    fun <Input : Any, Output : Any> start(
+        input: Observable<Input>,
+        formula: IFormula<Input, Output>,
+        config: RuntimeConfig?,
+    ): Observable<Output> {
+        return Observable.create<Output> { emitter ->
+            val runtime = FormulaRuntime(
+                formula = formula,
+                onOutput = emitter::onNext,
+                onError = emitter::onError,
+                config = config ?: RuntimeConfig(),
+            )
+
+            val disposables = CompositeDisposable()
+            disposables.add(input.subscribe(runtime::onInput, emitter::onError))
+            disposables.add(Disposable.fromAction(runtime::terminate))
+
+            emitter.setDisposable(disposables)
+        }.distinctUntilChanged()
+    }
+}
--- formula-rxjava3/src/main/java/com/instacart/formula/rxjava3/RxJavaRuntimeExtensions.kt
+++ formula-rxjava3/src/main/java/com/instacart/formula/rxjava3/RxJavaRuntimeExtensions.kt
@@ -2,28 +2,28 @@ package com.instacart.formula.rxjava3
 
 import com.instacart.formula.IFormula
 import com.instacart.formula.RuntimeConfig
-import com.instacart.formula.toFlow
 import io.reactivex.rxjava3.core.Observable
-import kotlinx.coroutines.rx3.asFlow
-import kotlinx.coroutines.rx3.asObservable
 
 fun <Output : Any> IFormula<Unit, Output>.toObservable(
     config: RuntimeConfig? = null,
 ): Observable<Output> {
-    return toFlow(config).asObservable()
+    return toObservable(input = Unit, config)
 }
 
 fun <Input : Any, Output : Any> IFormula<Input, Output>.toObservable(
     input: Input,
     config: RuntimeConfig? = null,
 ): Observable<Output> {
-    return toFlow(input, config).asObservable()
+    return toObservable(input = Observable.just(input), config)
 }
 
 fun <Input : Any, Output : Any> IFormula<Input, Output>.toObservable(
     input: Observable<Input>,
     config: RuntimeConfig? = null,
 ): Observable<Output> {
-    val inputFlow = input.asFlow()
-    return toFlow(inputFlow, config).asObservable()
+    return RxJavaRuntime.start(
+        input = input,
+        formula = this,
+        config = config,
+    )
 }
