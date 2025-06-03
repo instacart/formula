d6a842b061fd2809df91dcae485134433a5c3388
Laimonas Turauskas <laimiux@gmail.com>
2025-06-03 16:47:09 -0400

    Update test action API.

--- formula-test/src/main/java/com/instacart/formula/test/TestRuntimeExtensions.kt
+++ formula-test/src/main/java/com/instacart/formula/test/TestRuntimeExtensions.kt
@@ -5,6 +5,19 @@ import com.instacart.formula.IFormula
 import com.instacart.formula.RuntimeConfig
 import com.instacart.formula.plugin.Dispatcher
 import com.instacart.formula.plugin.Inspector
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.async
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.TestCoroutineScheduler
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlin.coroutines.coroutineContext
 
 /**
  * An extension function to create a [TestFormulaObserver] for a [IFormula] instance.
@@ -25,5 +38,26 @@ fun <Input : Any, Output : Any, F: IFormula<Input, Output>> F.test(
     return TestFormulaObserver(runtimeConfig, this)
 }
 
-fun <Event> Action<Event>.test() = TestActionObserver(this)
+suspend fun <Event> Action<Event>.test(
+    callback: suspend TestActionObserver<Event>.() -> Unit,
+) {
+    val scope = newScope()
+    try {
+        val observer = TestActionObserver(this@test, scope)
+        callback(observer)
+    } finally {
+        scope.cancel()
+    }
+}
+
+private suspend fun newScope(): CoroutineScope {
+    val dispatcher = getDispatcher()
+    return CoroutineScope(coroutineContext + SupervisorJob() + dispatcher)
+}
+
+@OptIn(ExperimentalCoroutinesApi::class) // UnconfinedTestDispatcher is still experimental.
+private suspend fun getDispatcher(): CoroutineDispatcher {
+    // Use test-specific unconfined if test scheduler is in use to inherit its virtual time.
+    return coroutineContext[TestCoroutineScheduler]?.let(::UnconfinedTestDispatcher) ?: Dispatchers.Unconfined
+}
 
