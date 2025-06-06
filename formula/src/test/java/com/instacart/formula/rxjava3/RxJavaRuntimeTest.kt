40940cb6e0c74577e7a9d0d50e5aa7fd640a033a
Laimonas Turauskas <laimiux@gmail.com>
2025-06-06 11:24:59 -0400

    Revert RxJava runtime. (#434)

--- formula/src/test/java/com/instacart/formula/rxjava3/RxJavaRuntimeTest.kt
+++ formula/src/test/java/com/instacart/formula/rxjava3/RxJavaRuntimeTest.kt
@@ -2,10 +2,15 @@ package com.instacart.formula.rxjava3
 
 import com.google.common.truth.Truth
 import com.google.common.truth.Truth.assertThat
+import com.instacart.formula.Action
+import com.instacart.formula.Evaluation
+import com.instacart.formula.Formula
 import com.instacart.formula.RuntimeConfig
+import com.instacart.formula.Snapshot
 import com.instacart.formula.test.CountingInspector
 import com.instacart.formula.types.InputIdentityFormula
 import io.reactivex.rxjava3.core.Observable
+import kotlinx.coroutines.flow.flowOf
 import org.junit.Test
 
 class RxJavaRuntimeTest {
@@ -33,5 +38,25 @@ class RxJavaRuntimeTest {
         val input = Observable.just(0, 1, 2)
         val observer = formula.toObservable(input).test()
         assertThat(observer.values().last()).isEqualTo(2)
+    }
+
+    @Test fun `incorporate flow action with rxjava runtime`() {
+        val formula = object : Formula<Unit, Int, Int>() {
+            override fun initialState(input: Unit): Int = 0
+
+            override fun Snapshot<Unit, Int>.evaluate(): Evaluation<Int> {
+                return Evaluation(
+                    output = state,
+                    actions = context.actions {
+                        Action.fromFlow { flowOf(1, 2) }.onEvent {
+                            transition(it)
+                        }
+                    }
+                )
+            }
+        }
+
+        val observer = formula.toObservable().test()
+        observer.assertValues(2)
     }
 }
