4b1bf1b678aa40c148d43caab5b6b8da7e058745
alexanderbezverhni <bezverhni@gmail.com>
2026-05-13 13:46:20 -0700

    Remove NeverEmitStateFlow.

--- test-utils/android/src/main/java/com/instacart/testutils/android/NoOpFeatureFactory.kt
+++ test-utils/android/src/main/java/com/instacart/testutils/android/NoOpFeatureFactory.kt
@@ -4,27 +4,16 @@ import com.instacart.formula.android.Feature
 import com.instacart.formula.android.FeatureFactory
 import com.instacart.formula.android.RouteKey
 import com.instacart.formula.android.ViewFactory
-import kotlinx.coroutines.awaitCancellation
-import kotlinx.coroutines.flow.FlowCollector
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class NoOpFeatureFactory<FragmentKeyT : RouteKey>(
-    private val viewFactory: ViewFactory<FragmentKeyT> = TestViewFactory(),
+    private val viewFactory: ViewFactory<Unit> = TestViewFactory(),
 ) : FeatureFactory<Unit, FragmentKeyT>() {
-    @Suppress("UNCHECKED_CAST")
     override fun Params.initialize(): Feature {
         return Feature(
             viewFactory = viewFactory,
         ) {
-            NeverEmitStateFlow as StateFlow<FragmentKeyT>
+            MutableStateFlow(Unit)
         }
-    }
-}
-
-private object NeverEmitStateFlow : StateFlow<Any> {
-    override val value: Any get() = Unit
-    override val replayCache: List<Any> = emptyList()
-    override suspend fun collect(collector: FlowCollector<Any>): Nothing {
-        awaitCancellation()
     }
 }
